68a9d1d32af88ea16a506a765953b55b73ae8a40
tushar-454 <imtushar454@gmail.com>
2024-12-10 11:50:25 +0600

    initialize TypeScript Express application with basic routing and middleware

---
+++ src/middlewares/index.ts
@@ -0,0 +1,24 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Application } from 'express';
+import globalError from '../middlewares/globalError';
+import logger from '../middlewares/logger';
+import routes from '../routes';
+
+const applyMiddleware = (app: Application): void => {
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL,
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    })
+  );
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(logger);
+  app.use(routes);
+  app.use(globalError);
+};
+
+export default applyMiddleware;
