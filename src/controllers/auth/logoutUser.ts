f611b794c1972215f70f4ff5dbb99fe227fcff30
tushar-454 <imtushar454@gmail.com>
2024-12-13 08:49:04 +0600

    feat: enhance logoutUser functionality to clear cookie with secure options

--- src/controllers/auth/logoutUser.ts
+++ src/controllers/auth/logoutUser.ts
@@ -2,7 +2,12 @@ import { NextFunction, Request, Response } from 'express';
 
 const logoutUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    res.clearCookie('token');
+    res.clearCookie('token', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      path: '/',
+    });
     res.status(200).json({
       status: 200,
       message: 'Logged out successfully',
